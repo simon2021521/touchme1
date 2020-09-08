696c654321784c6ea66f810dd8b4926323a9a1fc
Felix Qin <felixq@Felixs-MBP.modem>
2020-09-08 14:37:00 +1000

    Initial commit

---
+++ app/src/main/java/com/example/touchme1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.touchme1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayStrings(View view){
+        TextView str1 = (TextView) findViewById(R.id.strOne);
+        TextView str2 = (TextView) findViewById(R.id.strTwo);
+        str1.setText("Rmit");
+        str2.setText("Mel");
+    }
+
+}//end fo app
